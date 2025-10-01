d3d903851b5b4f3bf480f219afb6a20cd56944cd
Gustavo Bercacollo Vilela <gbercaco@c1r10p3.42sp.org.br>
2025-10-01 17:07:42 -0300

    ok

--- src/ft_ctype/ft_isalnum.c
+++ src/ft_ctype/ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: gbercaco <gbercaco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 17:23:30 by gbercaco          #+#    #+#             */
-/*   Updated: 2025/07/25 19:36:25 by gbercaco         ###   ########.fr       */
+/*   Updated: 2025/10/01 17:06:38 by gbercaco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 ** Returns 1 if it is a letter (A-Z, a-z) or digit (0-9), otherwise 0.
 */
 
-#include "libft.h"
+#include "ft_ctype.h"
 
 int	ft_isalnum(int c)
 {
