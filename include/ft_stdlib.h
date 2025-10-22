ff7b6333f8fffb468db8ff0813244e83303463e5
Gustavo Bercacollo Vilela <gbercaco@c2r11p6.42sp.org.br>
2025-10-22 16:04:41 -0300

    fix norminette

--- include/ft_stdlib.h
+++ include/ft_stdlib.h
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stdlib.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbercaco <gbercaco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 15:52:56 by gbercaco          #+#    #+#             */
+/*   Updated: 2025/10/22 15:52:56 by gbercaco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_STDLIB_H
 # define FT_STDLIB_H
 
 # include <stdlib.h>
-#include "ft_string.h"
+# include "ft_string.h"
 
 int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
